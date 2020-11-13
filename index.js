97f5b20d3344b9f938ead4743d0b0c3536ca7654
alp-ex <mail.alp.ex@gmail.com>
2020-11-13 14:58:29 +0100

    we now have users

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { random } = require("faker");
+const { random, internet } = require("faker");
 
 const LIMIT = 1000;
 
 module.exports = () => {
-  const data = { mealPlan: [], recipes: [], categories: [] };
+  const data = { mealPlan: [], recipes: [], categories: [], users: [] };
   const categories = [
     { name: "desserts", priorityLevel: 2 },
     { name: "starters", priorityLevel: 0 },
@@ -11,8 +11,17 @@ module.exports = () => {
   ];
 
   for (let i = 0; i < 100; i++) {
+    data.users.push({
+      id: i,
+      email: internet.email(),
+      password: internet.password(),
+    });
+  }
+
+  for (let i = 0; i < 100; i++) {
     data.mealPlan.push({
       id: i,
+      usersId: random.number({ min: 0, max: LIMIT }),
       recipesId: random.number({ min: 0, max: LIMIT }),
       weekday: random.number({ min: 0, max: 7 }),
     });
@@ -26,9 +35,10 @@ module.exports = () => {
     });
   });
 
-  for (let i = 0; i < LIMIT; i++) {
+  for (let i = 0; i < LIMIT * 5; i++) {
     data.recipes.push({
       id: i,
+      usersId: random.number({ min: 0, max: LIMIT }),
       name: random.word(),
       categories: random.arrayElement(categories).name,
       ingredients: random
